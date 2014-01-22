c609f531864e8e2a23f0c320c12eac765ee28644
dimitrigraf <dimitri.graf01@gmail.com>
2014-01-22 20:59:13 +0100

    manuals templates added, new sections added

---
+++ manuals/owncloud/build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+log()
+{
+  echo `date "+%Y.%m.%d %H:%M:%S[$$]:"` $@
+}
+
+log_error()
+{
+  echo -e "\033[1;31m`log $@`\033[1;m" >&2
+}
+
+
+SECTIONS_DIR=sections
+OUTPUT_DIR=output
+TEMP_DIR=build_tmp
+
+export TEMP_DIR
+
+# prepare
+if [[ ! -d "$TEMP_DIR" ]]; then
+  mkdir "$TEMP_DIR"
+fi
+
+if [[ ! -d "$OUTPUT_DIR" ]]; then
+  mkdir "$OUTPUT_DIR"
+else
+  rm -rf "$OUTPUT_DIR/*"
+fi
+
+# create sections include
+log "Create sections include"
+ls $SECTIONS_DIR/*.tex | sort -V | awk '{ printf "\\input{%s}\n", $1}' > $TEMP_DIR/sections.tex
+
+log "Create pdf from latex"
+pdflatex --halt-on-error --output-directory="$OUTPUT_DIR" document.tex > /dev/null
+if [[ $? -ne 0 ]]; then
+  log_error "Failed to create pdf from latex (see $OUTPUT_DIR/document.log)"
+fi
+
+# cleanup
+log "Cleanup temporary files"
+rm -rf "$TEMP_DIR"
